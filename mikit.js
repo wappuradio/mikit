9ba44c5ee51750415c62af580939efe81fadde77
Esa Varemo <git@esav.fi>
2023-04-18 23:39:11 +0300

    Switch from local MIDI connection to websocket

--- mikit.js
+++ mikit.js
@@ -1,52 +1,55 @@
-var midi = require('easymidi')
 var express = require('express')();
+var wsclient = require('websocket').client;
 var http = require('http').Server(express);
 var io = require('socket.io')(http);
 
-const controls = {
-  "UM-ONE": [ 64, 65, 66, 67 ]
-}
+const N_MICS = 8;
+var mics = {};
 
-const inputs = midi.getInputs();
-var input;
-var mics = [];
-var state = {};
+let ql = new wsclient();
 
-for(var key in controls) {
-  for(var i of inputs) {
-    if(i.match(key)) {
-      input = new midi.Input(i);
-      mics = controls[key];
-    }
-  }
-}
+ql.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+    process.exit(1);
+});
 
-if(!input) process.exit();
-if(mics.length == 0) process.exit();
-for(var i = 0; i < mics.length; i++) {
-  state[i+1] = false;
-}
+ql.on('connect', function(connection) {
+    console.log('QL API WS Connected');
+    connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+        process.exit(1);
+    });
+    connection.on('close', function() {
+        console.log('QL API WS Connection Closed');
+        process.exit(1);
+    });
+    connection.on('message', function(message) {
+        const packet = JSON.parse(message.utf8Data);
+        const {channel, state} = packet;
+        if (state !== null) {
+            mics[channel+1] = state;
+            bcast(channel+1, state);
+        }
+    });
+});
+ql.connect('ws://qltools.idm.wappuradio.fi:8083/', 'ql-json1');
 
-input.on('cc', (msg) => {
-  for(var i = 0; i < mics.length; i++) {
-    if(mics[i] == msg.controller) {
-      state[i+1] = msg.value>0;
-      bcast(i+1, msg.value>0);
-    }
-  }
-})
-
-var bcast = (n, state) => {
-  io.emit('msg', { mic: 1*n, state: state });
+for(var i = 0; i < N_MICS; i++) {
+  mics[i+1] = false;
 }
 
 express.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+var bcast = (n, state) => {
+  console.log("sending transition on ch", n, "to", state);
+  io.emit('msg', { mic: 1*n, state: state });
+}
+
 io.on('connection', (socket) => {
-  for(var i in state) {
-    bcast(i, state[i]);
+  for(var i in mics) {
+    bcast(i, mics[i]);
   }
 });
 
